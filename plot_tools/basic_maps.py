f4c2bd4824cd7a2839d3db25d6b4ba521bb5dbb3
Mirjam van der Mheen <mirjam.vandermheen@uwa.edu.au>
2024-06-26 14:06:46 +0800

    Added option to plot NE bathymetry

--- plot_tools/basic_maps.py
+++ plot_tools/basic_maps.py
@@ -2,12 +2,16 @@ import os, sys
 parent = os.path.abspath('.')
 sys.path.insert(1, parent)
 
+from tools.files import get_dir_from_json
 import cartopy.crs as ccrs
 import cartopy.mpl.ticker as cticker
 from cartopy.io import shapereader
 import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
+from matplotlib.colors import BoundaryNorm
+from matplotlib import colormaps
 import numpy as np
+from glob import glob
 
 lon_range_default = [110.0, 154.0]
 lat_range_default = [-46.0, -24.0]
@@ -75,6 +79,54 @@ def plot_bathymetry(lon:np.ndarray, lat:np.ndarray, h:np.ndarray,
     else:
         return ax, c, cbar
 
+def load_ne_bathymetry(input_dir=f'{get_dir_from_json("ne")}'):
+    # Read shapefiles, sorted by depth
+    shp_dict = {}
+    files = glob(f'{input_dir}ne_10m_bathymetry_all/*.shp')
+    assert len(files) > 0
+    files.sort()
+    depths = []
+    for f in files:
+        depth = '-' + f.split('_')[-1].split('.')[0]  # depth from file name
+        depths.append(depth)
+        bbox = (90, -15, 160, 60)  # (x0, y0, x1, y1)
+        nei = shapereader.Reader(f, bbox=bbox)
+        shp_dict[depth] = nei
+    depths = np.array(depths)[::-1]  # sort from surface to bottom
+    return depths, shp_dict
+
+def plot_ne_bathymetry(ax:plt.axes, lon_range=None, lat_range=None):
+    # Load data (14.8 MB file)
+    depths_str, shp_dict = load_ne_bathymetry()
+
+    # Construct a discrete colormap with colors corresponding to each depth
+    depths = depths_str.astype(int)
+    N = len(depths)
+    nudge = 0.01  # shift bin edge slightly to include data
+    boundaries = [min(depths)] + sorted(depths+nudge)  # low to high
+    norm = BoundaryNorm(boundaries, N)
+    blues_cm = colormaps['Blues_r'].resampled(N)
+    colors_depths = blues_cm(norm(depths))
+
+    # Iterate and plot feature for each depth level
+    for i, depth_str in enumerate(depths_str):
+        ax.add_geometries(shp_dict[depth_str].geometries(),
+                          crs=ccrs.PlateCarree(),
+                          color=colors_depths[i])
+
+    if lon_range is None:
+        lon_range = lon_range_default
+    if lat_range is None:
+        lat_range = lat_range_default
+    
+    ax.set_extent([lon_range[0], lon_range[1],
+                   lat_range[0], lat_range[1]],
+                   ccrs.PlateCarree())
+
+    # Convert vector bathymetries to raster (saves a lot of disk space)
+    # while leaving labels as vectors
+    ax.set_rasterized(True)
+
 def add_grid(ax:plt.axes, meridians:list, parallels:list,
               xmarkers:str, ymarkers:str, draw_grid:bool) -> plt.axes:
 
@@ -103,7 +155,8 @@ def add_grid(ax:plt.axes, meridians:list, parallels:list,
 def plot_basic_map(ax:plt.axes, lon_range=None, lat_range=None,
                    meridians=None, parallels=None,
                    xmarkers='bottom', ymarkers='left',
-                   draw_grid=False, full_resolution=False) -> plt.axes:
+                   draw_grid=False, full_resolution=False,
+                   show_reefs=False) -> plt.axes:
     
     if lon_range is None:
         lon_range = lon_range_default
@@ -120,6 +173,15 @@ def plot_basic_map(ax:plt.axes, lon_range=None, lat_range=None,
     else:
         ax.add_feature(cfeature.LAND, edgecolor='k', facecolor='#d2d2d2', zorder=2)
         ax.add_feature(cfeature.COASTLINE, zorder=2)
+    
+    if show_reefs == True:
+        reefs = cfeature.NaturalEarthFeature(
+            category='physical',
+            name='reefs',
+            scale='10m',
+            facecolor='#d2d2d2')
+        
+        ax.add_feature(reefs, edgecolor='k', facecolor='#d2d2d2', zorder=2)
     
     ax = add_grid(ax, meridians, parallels, xmarkers, ymarkers, draw_grid)
 
